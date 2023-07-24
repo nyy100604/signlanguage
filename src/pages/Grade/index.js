17c078c84fe54b464f9869ac6e82c1e2c60e549f
class-2022 <40971203H@gapps.ntnu.edu.tw>
2023-07-25 01:31:52 +0800

    adjust

--- src/pages/Grade/index.js
+++ src/pages/Grade/index.js
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import NavComponents from "../../components/NavComponents";
 import Footer from "../../components/Footer";
-
+import axios from "axios";
 
 const Grade = () => {
-    const units = ['單元一', '單元二', '單元三']; //查詢應該會改成123比較好查
+    const units = ['unit1', 'unit2', 'unit3']; 
 
     const [selectedUnit, setSelectedUnit] = useState(units[0]); // 預設選擇第一個單元
     const [gradesData, setGradesData] = useState({});
@@ -14,17 +14,35 @@ const Grade = () => {
         setSelectedUnit(event.target.value);
     };
 
+    async function getGrade() {
+        var id = localStorage.getItem("id");
+        console.log("hello", id);
+        id = String(id)
+
+        var formData = new FormData();
+        formData.append('user_id', id)
+        formData.append("unit", selectedUnit)
+
+        try {
+            const response = await axios.post(
+                "http://localhost:5000/Grade",
+                formData,
+                {
+                    headers: {
+                        "Content-Type": "multipart/form-data",
+                    },
+                }
+            );
+            console.log(response.data);
+            setGradesData(response.data);
+        } catch (error) {
+            console.error('獲取成績資料時發生錯誤：', error);
+        }
+    }
+
+
     useEffect(() => {
-        // 在這裡發送API請求來獲取成績資料，假設後端提供了一個獲取成績資料的API端點：/api/grades
-        fetch(`/api/grades?unit=${selectedUnit}`)
-            .then((response) => response.json())
-            .then((data) => {
-                // 假設資料回傳的格式為 { word: grade } 的物件
-                setGradesData(data);
-            })
-            .catch((error) => {
-                console.error('獲取成績資料時發生錯誤：', error);
-            });
+       getGrade()
     }, [selectedUnit]);
 
     const selectedUnitWordsAndGrades = selectedUnit in gradesData ? Object.entries(gradesData[selectedUnit]) : [];
